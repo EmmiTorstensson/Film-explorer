215a1caab8cab998b829d50601f2d372103c2bde
”Emmi <emmitorstensson@hotmail.com>
2024-12-08 23:39:57 +0100

    last push

--- src/app/api/movies/[id]/route.ts
+++ src/app/api/movies/[id]/route.ts
@@ -1,22 +1,22 @@
-import axios from "axios";
-import { NextResponse } from "next/server";
+	import axios from "axios";
+	import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-	const apiKey = process.env.TMDB_API_KEY;
-	const movieId = params.id;
+	export async function GET(request: Request, { params }: { params: { id: string } }) {
+		const apiKey = process.env.TMDB_API_KEY;
+		const movieId = params.id;
 
-	try {
-		const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
-			params: {
-				api_key: apiKey,
-				language: 'eng-US'
-			},
-		});
+		try {
+			const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
+				params: {
+					api_key: apiKey,
+					language: 'eng-US'
+				},
+			});
 
-		return NextResponse.json(response.data)
+			return NextResponse.json(response.data)
 
-	} catch(error) {
-		console.error("Error getting movie details", error.response ? error.response.data : error.message);
-		return NextResponse.json({ error: "Faild to get movie details" }, { status: 500 })
+		} catch(error) {
+			console.error("Error getting movie details", error.response ? error.response.data : error.message);
+			return NextResponse.json({ error: "Faild to get movie details" }, { status: 500 })
+		}
 	}
-}
